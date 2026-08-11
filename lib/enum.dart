f1ad1b45d735a7cc043d09a46bfd8ade6ae9f4d6
InterActive IOS Developer <developer_002@interactive.co.id>
2026-08-11 16:55:03 +0700

    update: assets manager from external assets repository

---
+++ lib/enum.dart
@@ -0,0 +1,25 @@
+// model onnx
+List<String> modelList = [
+  "https://storage.googleapis.com/storage_interactive/interactive/mobile/tts-engine/onnx/tts.json",
+  "https://storage.googleapis.com/storage_interactive/interactive/mobile/tts-engine/onnx/unicode_indexer.json",
+  "https://storage.googleapis.com/storage_interactive/interactive/mobile/tts-engine/onnx/duration_predictor.onnx",
+  "https://storage.googleapis.com/storage_interactive/interactive/mobile/tts-engine/onnx/text_encoder.onnx",
+  "https://storage.googleapis.com/storage_interactive/interactive/mobile/tts-engine/onnx/vector_estimator.onnx",
+  "https://storage.googleapis.com/storage_interactive/interactive/mobile/tts-engine/onnx/vocoder.onnx",
+];
+
+
+//voice styles
+List<String> voicesStyles = [
+  "https://storage.googleapis.com/storage_interactive/interactive/mobile/tts-engine/voice_styles/M1.json",
+  "https://storage.googleapis.com/storage_interactive/interactive/mobile/tts-engine/voice_styles/M2.json",
+  "https://storage.googleapis.com/storage_interactive/interactive/mobile/tts-engine/voice_styles/M3.json",
+  "https://storage.googleapis.com/storage_interactive/interactive/mobile/tts-engine/voice_styles/M4.json",
+  "https://storage.googleapis.com/storage_interactive/interactive/mobile/tts-engine/voice_styles/M5.json",
+  "https://storage.googleapis.com/storage_interactive/interactive/mobile/tts-engine/voice_styles/F1.json",
+  "https://storage.googleapis.com/storage_interactive/interactive/mobile/tts-engine/voice_styles/F2.json",
+  "https://storage.googleapis.com/storage_interactive/interactive/mobile/tts-engine/voice_styles/F3.json",
+  "https://storage.googleapis.com/storage_interactive/interactive/mobile/tts-engine/voice_styles/F4.json",
+  "https://storage.googleapis.com/storage_interactive/interactive/mobile/tts-engine/voice_styles/F5.json",
+];
+
